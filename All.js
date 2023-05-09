bc33a5b5fb886f0f9a78b190f3db581f6f715dff
Henrique <123227224+RafaelHenriqu@users.noreply.github.com>
2023-05-09 18:47:26 +0100

    V0.0.6
    Modo DarkMod
    Com salvamento Usando Localstorage

--- All.js
+++ All.js
@@ -1,4 +1,22 @@
+let Html = document.querySelector('html')
 function All()
 {
-    
+ 
+if (localStorage.Tema == 'Black')
+{
+    Html.style.backgroundImage = 'linear-gradient(to left, #02cfb2, #0f21bc)'
+localStorage.Tema = 'White'
+}else
+{
+    Html.style.backgroundImage = 'linear-gradient(to left, #191920, #0f21bc)'
+    localStorage.Tema = 'Black'
+}
+}
+
+if (localStorage.Tema == 'Black')
+{ 
+    Html.style.backgroundImage = 'linear-gradient(to left, #191920, #0f21bc)'
+}else
+{   
+    Html.style.backgroundImage = 'linear-gradient(to left, #02cfb2, #0f21bc)'
 }
