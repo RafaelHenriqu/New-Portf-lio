c000f2cf812437a0f3294da4154d2e3299d18cca
Henrique <123227224+RafaelHenriqu@users.noreply.github.com>
2023-05-10 01:44:20 +0100

    EsterEggUpdate

--- All.js
+++ All.js
@@ -15,7 +15,11 @@ localStorage.Tema = 'White'
 
 if (localStorage.Tema == 'Black')
 { 
+    let Perfil = document.getElementById('Perfil')
     Html.style.backgroundImage = 'linear-gradient(to left, #191920, #0f21bc)'
+    Perfil.style.backgroundImage = 'url(img/RinOkumura.gif)'
+    Perfil.style.border = '2px solid blue'
+    Perfil.style.boxShadow = '0px 0px 50px blue'
 }else
 {   
     Html.style.backgroundImage = 'linear-gradient(to left, #02cfb2, #0f21bc)'
